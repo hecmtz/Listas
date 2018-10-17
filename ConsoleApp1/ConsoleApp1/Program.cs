584570bc00098f36c66d94bdacf128c4a06ab586
Hector Martinez <43742306+hecmtz@users.noreply.github.com>
2018-10-17 13:15:53 +0200

    ListaSuper

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listas
+{
+    class ProgramListas
+    {
+        static void Main(string[] args)
+        {
+            string articulo = "Manzanas";
+            List<String> compra = new List<String>();
+            CrearLista(compra);
+            MostrarLista(compra);
+            OrdenarLista(compra);
+            BorrarLista(compra, articulo);
+            MostrarLista(compra);
+            Console.WriteLine(ContarElementos(compra));
+            Console.ReadLine();
+        }
+        public static void CrearLista(List<string> compra)
+        {
+            compra.Add("Manzanas");
+            compra.Add("Carne");
+
+        }
+        public static void MostrarLista(List<string> compra)
+        {
+            foreach (String x in compra)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine(" ");
+        }
+        public static void OrdenarLista(List<string> compra)
+        {
+            //compra.OrderBy(o => o).ToList();
+            compra.Sort();
+            foreach (String x in compra)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine(" ");
+        }
+
+        public static void BorrarLista(List<string> compra, string articulo) //hola
+        {
+            compra.Remove(articulo);
+        }
+        public static int ContarElementos(List<string> compra)
+        {
+            return compra.Count;
+            //comentarios
+        }
+    }
+}
